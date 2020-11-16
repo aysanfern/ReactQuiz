1b17e40834a0b94fc44b78634b7fdeaf714100d7
Aysan Fernandes <47918314+aysanfern@users.noreply.github.com>
2020-11-16 17:08:43 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,113 @@
+import {useState,useEffect} from 'react';
+import { Button, Jumbotron } from 'react-bootstrap';
+import {Form1} from './components/Form1'
+import {Form2} from './components/Form2'
+
+
+function App() {
+  const [answer, setAnswer] = useState('hey');
+  const [yes,setYes]= useState(null);
+  const [question,setQuestion] = useState(0);
+  const [last,setLast]=useState(true)
+
+  const myStyle={
+    backgroundColor: "lightblue",
+    textAlign:'center'
+    
+  }
+  
+  useEffect(()=>{
+    if (number===(question+1)){
+      setLast(true)
+    }
+    else
+    {
+      setLast(false)
+    }
+  })
+
+
+  const form1 = (<Form1
+    handleNext = {handleNext}
+    handlePrev={handlePrev}
+    question = {question}
+    last={last}
+    />)
+  const form2 =(
+    <Form2
+    handleNext = {handleNext}
+    handlePrev={handlePrev}
+    question = {question}
+    last={last}
+    />)
+
+
+  const array=[form1,form2]
+  const number = array.length
+
+  function handleClick(event){
+    setAnswer(answer => event.target.value);
+    if (answer==='correct'){
+      setYes(true)
+    }
+    else
+    {
+      setYes(false)
+    }
+    
+
+  }
+
+  function handleNext(){
+    setQuestion(q => q+1)
+  }
+  function handlePrev(){
+    setQuestion(q => q-1)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    if (answer==='correct'){
+      setYes(true)
+    }
+    else
+    {
+      setYes(false)
+    }
+
+
+  }
+
+  return (
+    <div className="App">
+      <Jumbotron>
+
+   
+    <h1   style = {myStyle}>
+        Question {question+1} of {array.length}
+        </h1>
+  
+  {array[question]}
+
+  </Jumbotron>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+  );
+}
+
+export default App;
